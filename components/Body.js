56195e0a2be36006ba0b111aedff98b1be9cf96a
ruchik02 <ruchikasharma.cse19@chitkarauniversity.edu.in>
2022-11-21 15:38:44 +0530

    get the term from the input and redirect to search

--- components/Body.js
+++ components/Body.js
@@ -1,7 +1,17 @@
 import Image from "next/image";
 import { AiOutlineSearch } from "react-icons/ai";
 import { MdKeyboardVoice } from "react-icons/md";
+import { useRef } from "react";
+import { useRouter } from "next/router";
 const Body = () => {
+  const router=useRouter();
+  const searchInputRef=useRef(null);
+  const serach=(e)=>{
+    e.preventDefault();
+    const term=searchInputRef.current.value;
+    if(!term.trim()) return 
+     router.push(`/search?term=${term.trim()}`)
+  }
   return (
     <>
       <form className="flex flex-col items-center mt-40">
@@ -13,11 +23,11 @@ const Body = () => {
         />
         <div className="flex w-full mt-5 mx-auto max-w-[90%] border border-gary-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-xl lg:max-w-2x">
           <AiOutlineSearch className="h-5 text-gray-500 mr-3" />
-          <input type="text" className="flex-grow focus:outline-none" />
+          <input ref={searchInputRef} type="text" className="flex-grow focus:outline-none" />
           <MdKeyboardVoice className="h-5" />
         </div>
         <div className="flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
-          <button className="btn">Google Search</button>
+          <button onClick={serach} className="btn">Google Search</button>
           <button className="btn">I'm Feeling Lucky </button>
         </div>
       </form>
--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <>
-      <footer className="flex justify-center items-center whitespace-nowrap p-6 text-sm text-gray-600 mt-64">
+      <footer className="flex justify-center items-center whitespace-nowrap p-6 text-sm text-gray-600 mt-60">
         <p>Copyright &copy; {new Date().getFullYear()} Ruchika Sharma</p>
       </footer>
     </>
